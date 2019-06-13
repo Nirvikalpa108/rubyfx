683e71afd904c113865a32f1049e7d7c193dd024
Nirvikalpa108 <amina.adewusi@gmail.com>
2019-06-13 09:02:25 +0100

    adds rate method without base EUR calculation

--- lib/currency_exchange.rb
+++ lib/currency_exchange.rb
@@ -1,10 +1,16 @@
+require 'json'
+require 'date'
+
 module CurrencyExchange
 
-  # Return the exchange rate between from_currency and to_currency on date as a float.
-  # Raises an exception if unable to calculate requested rate.
-  # Raises an exception if there is no rate for the date provided.
   def self.rate(date, from_currency, to_currency)
-    # TODO: calculate and return rate
+    file_name = 'data/eurofxref-hist-90d.json'
+    nested_hash = JSON.parse(File.read(file_name))
+    hash = nested_hash[date.strftime]
+    hash[to_currency] / hash[from_currency]
   end
-
 end
+
+
+# Raises an exception if unable to calculate requested rate.
+# Raises an exception if there is no rate for the date provided.
