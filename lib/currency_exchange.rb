6be23d5f96d2f163e2c7d7605ee66a5d554c2d89
Nirvikalpa108 <amina.adewusi@gmail.com>
2019-06-16 21:15:00 +0100

    adds default hash value for EUR only

--- lib/currency_exchange.rb
+++ lib/currency_exchange.rb
@@ -7,7 +7,9 @@ module CurrencyExchange
     file_name = 'data/eurofxref-hist-90d.json'
     nested_hash = JSON.parse(File.read(file_name))
     hash = nested_hash[date.strftime]
-    hash.default = 1
+    if to_currency == "EUR"
+      hash.default = 1
+    end
     hash[to_currency] / hash[from_currency]
   end
 end
