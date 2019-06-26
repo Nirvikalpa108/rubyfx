5925f790c25ac55bcf131f577f322bc99d88f2e9
Nirvikalpa108 <amina.adewusi@gmail.com>
2019-06-27 03:06:35 +0800

    begins refactoring

--- lib/currency_exchange.rb
+++ lib/currency_exchange.rb
@@ -1,15 +1,40 @@
 require 'json'
 require 'date'
 
-module CurrencyExchange
+class CurrencyExchange
+
+  class CurrencyError < StandardError
+  end
+
+  class DateError < StandardError
+  end
+
+  class RateCalculate
+    attr_accessor :date
+
+    def initialise(date)
+      @date = date
+    end
+
+    def convert(date, from_currency, to_currency)
+      hash = nested_hash[date.strftime]
+      if to_currency == "EUR"
+        hash.default = 1
+      end
+      hash[to_currency] / hash[from_currency]
+    end
+
+    private
+    def nested_hash
+      JSON.parse(File.read(file_name))
+    end
+
+    def file_name
+      'data/eurofxref-hist-90d.json'
+    end
+  end
 
   def self.rate(date, from_currency, to_currency)
-    file_name = 'data/eurofxref-hist-90d.json'
-    nested_hash = JSON.parse(File.read(file_name))
-    hash = nested_hash[date.strftime]
-    if to_currency == "EUR"
-      hash.default = 1
-    end
-    hash[to_currency] / hash[from_currency]
+    RateCalculate.new.convert(date, from_currency, to_currency)
   end
 end
