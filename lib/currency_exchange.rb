afd0a1e96d1d71f6564ca17f27514f1c8956b63f
Nirvikalpa108 <amina.adewusi@gmail.com>
2019-06-27 22:39:34 +0800

    removes nested classes

--- lib/currency_exchange.rb
+++ lib/currency_exchange.rb
@@ -2,15 +2,7 @@ require 'json'
 require 'date'
 
 class CurrencyExchange
-
-  class CurrencyError < StandardError
-  end
-
-  class DateError < StandardError
-  end
-
-  class RateCalculate
-    def convert(date, from_currency, to_currency)
+    def self.rate(date, from_currency, to_currency)
       hash = parse_file[date.strftime]
       if to_currency == "EUR"
         hash.default = 1
@@ -18,17 +10,17 @@ class CurrencyExchange
       hash[to_currency] / hash[from_currency]
     end
 
-    private
-    def parse_file
+    def self.parse_file
       JSON.parse(File.read(file_name))
     end
 
-    def file_name
+    def self.file_name
       'data/eurofxref-hist-90d.json'
     end
-  end
+end
 
-  def self.rate(date, from_currency, to_currency)
-    RateCalculate.new.convert(date, from_currency, to_currency)
-  end
+class CurrencyError < StandardError
+end
+
+class DateError < StandardError
 end
