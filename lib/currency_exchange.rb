9be0da54d380dc2e3e2c70d3253f500d7bd0dfd1
Nirvikalpa108 <amina.adewusi@gmail.com>
2019-06-27 23:56:49 +0800

    Improves method name

--- lib/currency_exchange.rb
+++ lib/currency_exchange.rb
@@ -6,14 +6,14 @@ class CurrencyExchange
   FILE_NAME = 'data/eurofxref-hist-90d.json'
 
     def self.rate(date:, from:, to:)
-      hash = parse_file[date.strftime]
+      hash = fx_dates[date.strftime]
       if to == "EUR"
         hash.default = 1
       end
       hash[to] / hash[from]
     end
 
-    def self.parse_file
+    def self.fx_dates
       JSON.parse(File.read(FILE_NAME))
     end
 end
