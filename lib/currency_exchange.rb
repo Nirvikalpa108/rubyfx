5bb4d204bb47c23bf306aecb5716b2c1248fdb24
Nirvikalpa108 <amina.adewusi@gmail.com>
2019-07-18 09:03:43 +0100

    [WIP] csv debugging

--- lib/currency_exchange.rb
+++ lib/currency_exchange.rb
@@ -15,28 +15,35 @@ class CurrencyExchange
       if File.extname(file) == ".json"
         JSON.parse(File.read(file))
       elsif File.extname(file) == ".csv"
-        CSV.parse(File.read(file), headers:true, converters: :numeric)
-        # headers:true, outputs CSV Table object
-        # converters: :numeric, converts strings to numbers
-        # https://www.rubyguides.com/2018/10/parse-csv-ruby/
-        else raise FileError
+        CSV.read(file, headers:true)
+      else raise FileError
       end
     end
 
     def self.date_convert(file, date)
-      fx_dates(file).fetch(date.strftime) { raise DateError }
+        fx_dates(file).fetch(date.strftime) { raise DateError }
     end
 
     def self.to_currency(file, date, to)
       if to == BASE_RATE
         return 1
-      else
+      elsif File.extname(file) == ".json"
         date_convert(file, date).fetch(to) { raise CurrencyError }
+      elsif File.extname(file) == ".csv"
+        csv_file = CSV.read(file, headers:true)
+        csv_to_rate = csv_file.to_a.collect { |col_1, col_2, col_3| col_3 if col_1 == date && col_2 == to}.compact
+        csv_to_rate.first.to_f
       end
     end
 
     def self.from_currency(file, date, from)
-      date_convert(file, date).fetch(from) { raise CurrencyError }
+      if File.extname(file) == ".json"
+        date_convert(file, date).fetch(from) { raise CurrencyError }
+      elsif File.extname(file) == ".csv"
+        csv_file = CSV.read(file, headers:true)
+        csv_from_rate = csv_file.to_a.collect { |col_1, col_2, col_3| col_3 if col_1 == date && col_2 == from}.compact
+        csv_from_rate.first.to_f
+      end
     end
 end
 
