e29add25166323b12e426d63bdb705b4055c0cd4
Nirvikalpa108 <amina.adewusi@gmail.com>
2019-07-21 21:32:25 +0100

    [WIP] extracts out the json logic

---
+++ lib/json_lookup.rb
@@ -0,0 +1,28 @@
+# require_relative 'error'
+# put error classes in separate files
+#
+class JsonLookup
+  def initialize(file, date, from, to)
+    @file = file
+    @date = date
+    @from = from
+    @to = to
+  end
+
+  def to_currency
+    date_format(file, date).fetch(to) { raise CurrencyError }
+  end
+
+  def from_currency
+    date_format(file, date).fetch(from) { raise CurrencyError }
+  end
+
+  private
+  def read(file)
+    JSON.parse(File.read(file))
+  end
+
+  def date_format(file, date)
+    read(file).fetch(date.strftime) { raise DateError }
+  end
+end
