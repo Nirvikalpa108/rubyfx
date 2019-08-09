afb4dbdd074c7bd7813b98bce5677d59d23ea225
Nirvikalpa108 <amina.adewusi@gmail.com>
2019-08-09 15:20:53 +0100

    corrects csv logic

--- lib/csv_lookup.rb
+++ lib/csv_lookup.rb
@@ -1,6 +1,7 @@
 require_relative 'error'
 require 'csv'
 
+
 class CsvLookup
   def initialize(file, date, from, to)
     @file = file
@@ -10,19 +11,29 @@ class CsvLookup
   end
 
   def to_currency
-    csv_file = read(@file)
-    csv_to_rate = csv_file.to_a.collect { |col_1, col_2, col_3| col_3 if col_1 == @date && col_2 == @to}.compact
-    csv_to_rate.first.to_f
+    found = data.find do |row|
+      Date.parse(row["Date"]) == @date && row["Currency"] == @to
+    end
+    found["FX Rate"].to_f
   end
 
   def from_currency
-    csv_file = read(@file)
-    csv_from_rate = csv_file.to_a.collect { |col_1, col_2, col_3| col_3 if col_1 == @date && col_2 == @from}.compact
-    csv_from_rate.first.to_f
+    found = data.find do |row|
+      Date.parse(row["Date"]) == @date && row["Currency"] == @from
+    end
+    found["FX Rate"].to_f
   end
 
   private
   def read(file)
     CSV.read(file, headers:true)
+  end
+
+  def data
+    @data = []
+    read(@file).each do |row|
+      @data << row
+    end
+    @data
   end
 end
