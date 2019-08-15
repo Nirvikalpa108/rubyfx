0752458dd9e161178e48a80a065ab39b122126bc
Nirvikalpa108 <amina.adewusi@gmail.com>
2019-08-15 09:18:57 +0100

    adds base rate test for CSV files

--- test/currency_exchange_test.rb
+++ test/currency_exchange_test.rb
@@ -31,14 +31,19 @@ class CurrencyExchangeTest < Test::Unit::TestCase
   end
 
   def test_exception_raised_if_no_rate_for_date
-   assert_raise(DateError.new("Error - no rate at date found")) do
+    assert_raise(DateError.new("Error - no rate at date found")) do
       CurrencyExchange.new(date: Date.new(2008,11,22), from: "GBP", to: "USD", file: 'data/eurofxref-hist-90d.json').rate
    end
   end
 
-  def test_currency_exchange_correct_for_CSV_file
+  def test_non_base_currency_exchange_correct_for_CSV_file
     correct_rate = 1.2870493690602498
     assert_equal correct_rate, CurrencyExchange.new(date: Date.new(2018,11,22), from: "GBP", to: "USD", file: 'data/eurofxref-hist-90d.csv').rate
+  end
+
+  def test_base_currency_exchange_correct_for_CSV_file
+    correct_rate = 0.007763975155279502
+    assert_equal correct_rate, CurrencyExchange.new(date: Date.new(2018,11,22), from: "JPY", to: "EUR", file: 'data/eurofxref-hist-90d.csv').rate
   end
 
   def test_exception_raised_for_non_JSON_and_CSV_file_types
